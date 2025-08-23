7b164591c0013bcc5aa4aa12b6a5fa9ba4a03257
Peyton Bighorse <162384210+peytonbighorse@users.noreply.github.com>
2025-08-22 18:20:29 -0700

    Added css to dictionary entries

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Dictionary from "./Dictionary";
 export default function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
         <h1>Dictionary</h1>
       </header>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,29 +4,48 @@ import axios from "axios";
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
   const [searchWord, setSearchWord] = useState("");
-  const [definition, setDefinition] = useState(null);
+  const [definitions, setDefinitions] = useState([]);
   const [phoenticPronunciation, setPhoenticPronunciation] = useState(null);
-  const [partOfSpeech, setPartOfSpeech] = useState(null);
-  const [example, setExample] = useState(null);
+
   function changeKeyword(event) {
     setKeyword(event.target.value);
   }
 
   function getDefinition(response) {
     console.log(response);
-    function checkExample() {
-      if (response.data.meanings[0].example) {
-        return `"${response.data.meanings[0].example}"`;
+    function checkExample(i) {
+      if (response.data.meanings[i].example) {
+        return `"${response.data.meanings[i].example}"`;
       } else {
         return null;
       }
     }
 
+    function returnMeanings() {
+      let meaningsArray = [...response.data.meanings];
+
+      return (
+        <div>
+          {meaningsArray.map(function (i, index) {
+            return (
+              <div className="definition-entry" key={index}>
+                <div className="part-of-speech">
+                  {response.data.meanings[index].partOfSpeech}
+                </div>
+                <div className="meaning">
+                  {response.data.meanings[index].definition}
+                </div>
+                <div className="example">{checkExample(index)}</div>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+
     setSearchWord(response.data.word);
-    setDefinition(response.data.meanings[0].definition);
+    setDefinitions(returnMeanings());
     setPhoenticPronunciation(response.data.phonetic);
-    setPartOfSpeech(response.data.meanings[0].partOfSpeech);
-    setExample(checkExample());
 
     return null;
   }
@@ -34,22 +53,33 @@ export default function Dictionary() {
     event.preventDefault();
     const apiKey = `aef1757e37906f8atc32b9da5odbc24a`;
     const apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    console.log(keyword);
-    console.log(apiKey);
     axios.get(apiUrl).then(getDefinition);
   }
+  function showQuestion() {
+    if (!searchWord) {
+      return <div className="question">What word do you want to look up?</div>;
+    } else {
+      return null;
+    }
+  }
+
   return (
-    <div className="container">
+    <div className="Dictionary">
+      {showQuestion()}
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={changeKeyword} />
+        <input
+          type="search"
+          autoFocus={true}
+          onChange={changeKeyword}
+          placeholder=" Search for a word"
+        />
         <input type="submit" />
       </form>
 
       <h2 className="search-word">{searchWord}</h2>
-      <p className="phoentic-pronunciation">{phoenticPronunciation}</p>
-      <p className="part-of-speech">{partOfSpeech}</p>
-      <p className="definition">{definition}</p>
-      <p className="example">{example}</p>
+      <div className="phonetic-pronunciation">{phoenticPronunciation}</div>
+
+      <div className="definition">{definitions}</div>
     </div>
   );
 }
